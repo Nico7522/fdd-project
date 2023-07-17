1fee9911d0012547410fde1a9ea0388714c68c38
Nicolas <nico.daddabbo7100@gmail.com>
2023-07-17 12:34:57 +0200

    ajout méthode pour fermer le menu quand on change de page

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -12,6 +12,7 @@ export default function Navbar() {
   };
 
   useEffect(() => {
+    
     const changeWidth = () => {
         setWidth(window.innerWidth)
         if (window.innerWidth > 500) {
@@ -25,14 +26,18 @@ export default function Navbar() {
         window.removeEventListener('resize', changeWidth)
     }
   }, [])
+
+  const closeMenu = () => {
+    setToggleMenu(false)
+  }
   return (
     <nav>
       {(toggleMenu || width > 500) && (
         <ul className={style["liste"]}>
-          <li id={style['img']} className={style["items"]}><NavLink to='/'><img src={Home} width={30} height={22} alt="" /></NavLink></li>
-          <li className={style["items"]}><NavLink to='/fruits'>Fruits</NavLink></li>
-          <li className={style["items"]}><NavLink to='/sabres'>Sabres</NavLink></li>
-          <li className={style["items"]}>Iles</li>
+          <li onClick={closeMenu} id={style['img']} className={style["items"]}><NavLink to='/'><img src={Home} width={30} height={22} alt="" /></NavLink></li>
+          <li onClick={closeMenu} className={style["items"]}><NavLink to='/fruits'>Fruits</NavLink></li>
+          <li onClick={closeMenu} className={style["items"]}><NavLink to='/sabres'>Sabres</NavLink></li>
+          <li onClick={closeMenu} className={style["items"]}>Iles</li>
         </ul>
       )}
 
