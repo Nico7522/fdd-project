1b73af0266c93778e65e02c5b3793a4ebdbf3306
Nicolas <nico.daddabbo7100@gmail.com>
2023-07-20 17:16:46 +0200

    test

--- src/utils/concat.tsx
+++ src/utils/concat.tsx
@@ -1,33 +1,62 @@
-import { useEffect, useState } from "react";
-import { useFetchSabre } from "../hooks/sabre";
-import sabreDetails from "./sabre-image.json";
-import { SabreResponse } from "../types/sabre";
-
+// import { useEffect, useState } from "react";
+// import { useFetchSabre } from "../hooks/sabre";
+// import sabreDetails from "./sabre-image.json";
+// import { SabreResponse } from "../types/sabre";
+import { useState } from "react";
+import style from "./style.module.css";
 export default function Test() {
-  const [allData, setAllData] = useState<SabreResponse[]>([]);
-  const [dataConcat, setDataConcat] = useState<SabreResponse[]>([]);
-  const { data } = useFetchSabre();
+  const [page1, setPage1] = useState(true);
+  const [page2, setPage2] = useState(false);
+  const [move, setMove] = useState(false);
 
-  useEffect(() => {
-    let temp: SabreResponse[] = [];
-    if (data) {
-      for (let i = 0; i < data.length; i++) {
-        temp.push(Object.assign(data[i], sabreDetails.sabreImage[i]));
-      }
-      setAllData(temp);
-    }
+  const movFirstDiv = () => {
+    return new Promise<void>((resolve, reject) => {
+      setPage1(false);
+      setTimeout(() => {
+        setMove(false);
+        setPage2(true);
+        resolve();
+      }, 2000);
+    });
+  };
 
-    
-}, [data]);
+  const divMoving = async () => {
+    await movFirstDiv();
+    setTimeout(() => {
+      console.log("dsdd");
 
+      setMove(true);
+    }, 50);
+  };
   return (
-    <div>
-      <h1>TEST</h1>
+    <div className={style["div"]}>
+      {page2 === false && (
+        <div
+          className={
+            style["divContainer"] + " " + (!page1 ? style["move"] : "")
+          }
+        >
+          <p>sqsqssqs</p>
+          <p>sqsqssqs</p>
+          <p>sqsqssqs</p>
+        </div>
+      )}
+      {page2 === true && (
+        <div
+          className={
+            style["divContainer2"] + " " + (move ? style["move-come"] : "")
+          }
+        >
+          <p>sqsqssqs</p>
+          <p>sqsqssqs</p>
+          <p>sqsqssqs</p>
+        </div>
+      )}
+
+      <button onClick={() => divMoving()}>move</button>
     </div>
   );
 }
-
-
 
 // useEffect(() => {
 //     let temp: SabreResponse[] = [];
@@ -44,7 +73,6 @@ export default function Test() {
 //     }
 //   }, [value, data, page]);
 
-
 // useEffect(() => {
 //     let temp: SabreResponse[] = [];
 //     if (data) {
@@ -53,10 +81,9 @@ export default function Test() {
 //       }
 //       setAllData(temp);
 //     }
-    
-    
+
 //   }, [data]);
-  
+
 //   useEffect(() => {
 //     console.log('coucou');
 //     if (data) {
@@ -70,3 +97,23 @@ export default function Test() {
 //   if (loading) {
 //     return <div className="loader"></div>;
 //   }
+//   const [allData, setAllData] = useState<SabreResponse[]>([]);
+//   const [dataConcat, setDataConcat] = useState<SabreResponse[]>([]);
+//   const { data } = useFetchSabre();
+
+//   useEffect(() => {
+//     let temp: SabreResponse[] = [];
+//     if (data) {
+//       for (let i = 0; i < data.length; i++) {
+//         temp.push(Object.assign(data[i], sabreDetails.sabreImage[i]));
+//       }
+//       setAllData(temp);
+//     }
+
+// }, [data]);
+
+//   return (
+//     <div>
+//       <h1>TEST</h1>
+//     </div>
+//   );
