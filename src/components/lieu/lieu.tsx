6b05e5fe5170a3e701e26a7f72144932697520a7
Nicolas <nico.daddabbo7100@gmail.com>
2023-07-22 11:36:58 +0200

    ajout test transition pour afficher filtre

--- src/components/lieu/lieu.tsx
+++ src/components/lieu/lieu.tsx
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from "react";
-import LieuDetails from '../../utils/ile-image.json'
-import style from "./style.module.css"
+import LieuDetails from "../../utils/ile-image.json";
+import style from "./style.module.css";
 import { Dark } from "../../App";
 import franceLogo from "../../images/france.png";
 import japonLogo from "../../images/japon.png";
@@ -8,25 +8,34 @@ import { LieuResponse } from "../../types/lieu";
 
 import LieuImages from "./lieux-image";
 export default function Lieu({
-  id, french_name, roman_name, sea_name, region_name,affiliation_name}: LieuResponse) {
-   
-    
-  const dark = useContext(Dark)
+  id,
+  french_name,
+  roman_name,
+  sea_name,
+  region_name,
+  affiliation_name,
+}: LieuResponse) {
+  const dark = useContext(Dark);
   const [modal, setModal] = useState<boolean>(false);
   return (
-    <div className={style["lieu-component"]}>
-      <h3 className={dark ? style['title-black'] : style['title-normal']}>
-        <img src={japonLogo} width={20} alt="" />
-        <br />
-        {roman_name}
-      </h3>
-      <h4 className={dark ? style['title-black'] : style['title-normal']}>
-        <img src={franceLogo} width={20} alt="" />
-        <br />
-        {french_name}
-      </h4>
-      <LieuImages LieuDetails={LieuDetails.ilesImage} frenchName={french_name} />
-
-    </div>
+    <>
+      <div></div>
+      <div className={style["lieu-component"]}>
+        <h3 className={dark ? style["title-black"] : style["title-normal"]}>
+          <img src={japonLogo} width={20} alt="" />
+          <br />
+          {roman_name}
+        </h3>
+        <h4 className={dark ? style["title-black"] : style["title-normal"]}>
+          <img src={franceLogo} width={20} alt="" />
+          <br />
+          {french_name}
+        </h4>
+        <LieuImages
+          LieuDetails={LieuDetails.ilesImage}
+          frenchName={french_name}
+        />
+      </div>
+    </>
   );
 }
--- src/containers/lieux/lieux-display.tsx
+++ src/containers/lieux/lieux-display.tsx
@@ -1,19 +1,33 @@
-import { useContext, useEffect, useState } from 'react'
-import style from './style.module.css'
+import { useContext, useEffect, useState } from "react";
+import style from "./style.module.css";
 import "../../App.css";
 
-import Lieu from '../../components/lieu/lieu'
-import { usefetchLieu } from '../../hooks/lieu'
-import { LieuResponse } from '../../types/lieu';
-import { Dark } from '../../App';
-import SearchBar from '../../components/searchbar/searchbar';
+import Lieu from "../../components/lieu/lieu";
+import { usefetchLieu } from "../../hooks/lieu";
+import { LieuResponse } from "../../types/lieu";
+import { Dark } from "../../App";
+import SearchBar from "../../components/searchbar/searchbar";
 export default function Lieux() {
-
   const { data, error, loading } = usefetchLieu();
   const [filteredData, setFilteredData] = useState<LieuResponse[]>([]);
   const [page, setPage] = useState<number>(12);
   const [value, setValue] = useState<string>("");
-  const dark = useContext(Dark)
+  const [filter, setFilter] = useState<string>("");
+  const [className, setClassName] = useState<boolean>(false);
+
+  console.log(filter);
+
+  const dark = useContext(Dark);
+
+  useEffect(() => {
+    console.log('coucou');
+    
+    if (filter !== "") {
+      setClassName(true);
+   
+    }
+   
+  }, [filter]);
   useEffect(() => {
     if (data) {
       const filtered = data.filter((sabre: LieuResponse) => {
@@ -29,8 +43,33 @@ export default function Lieux() {
 
   return (
     <>
-      <h1 className={dark ? style['title-black'] : style['title-normal']}>Les lieux</h1>
+      <h1 className={dark ? style["title-black"] : style["title-normal"]}>
+        Les lieux
+      </h1>
       <SearchBar value={value} setValue={setValue} />
+      <div className={style["multiselect-container"]}>
+        <div
+          className={
+            style["select-dropdown"] + " " + (dark ? style["dark"] : "")
+          }
+        >
+          <select onChange={(e) => setFilter(e.target.value)} name="" id="">
+            <option value="region">Océans</option>
+            <option value="oceans">Région</option>
+            <option value="grandline">Grand Line</option>
+            <option value="nouveau-monde">Nouveau Monde</option>
+          </select>
+        </div>
+        {filter === "oceans" && (
+          <div className={style["selected-item"]}>
+            <ul className={ style['base-ul'] + " " + (className ? style['show'] : " ")}>
+              <li>Nord Blue</li>
+              <li>Sud Blue</li>
+              <li>West Blue</li>
+            </ul>
+          </div>
+        )}
+      </div>
       <div className={style["lieux-container"]}>
         {filteredData?.slice(0, page).map((lieu: LieuResponse) => {
           return <Lieu key={lieu.id} {...lieu} />;
@@ -47,4 +86,3 @@ export default function Lieux() {
     </>
   );
 }
-
