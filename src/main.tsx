edd1fdf03d093c2250aa450d79400d97718eed5c
Nicolas <nico.daddabbo7100@gmail.com>
2023-10-01 10:11:59 +0200

    ajout web analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react'
 import * as ReactDOM  from 'react-dom/client'
+import { inject } from '@vercel/analytics';
 
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { routes } from './routes/route';
 
 const router = createBrowserRouter(routes)
-
+inject();
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     {/* <App /> */}
